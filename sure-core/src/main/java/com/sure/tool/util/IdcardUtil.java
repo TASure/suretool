c943c8b5516ee41e0d12bbeb1a9f3805e645c6bb
Nick <335069951@qq.com>
2026-09-10 12:42:04 +0800

    build: 工程化收尾 - License 头校验与 SpotBugs 静态扫描（批次 6/12：core util）

--- sure-core/src/main/java/com/sure/tool/util/IdcardUtil.java
+++ sure-core/src/main/java/com/sure/tool/util/IdcardUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2026 suretool contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sure.tool.util;
 
 import com.sure.tool.lang.PatternPool;
@@ -130,10 +145,10 @@ public class IdcardUtil {
 			return null;
 		}
 		if (PatternPool.ID_CARD_18.matcher(idCard).matches()) {
-			return ((idCard.charAt(16) - '0') % 2 == 1) ? "男" : "女";
+			return ((idCard.charAt(16) - '0') % 2 != 0) ? "男" : "女";
 		}
 		if (PatternPool.ID_CARD_15.matcher(idCard).matches()) {
-			return ((idCard.charAt(14) - '0') % 2 == 1) ? "男" : "女";
+			return ((idCard.charAt(14) - '0') % 2 != 0) ? "男" : "女";
 		}
 		return null;
 	}
